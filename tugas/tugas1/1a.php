6f6c8294b48ccf9ed559c1bd69fdcb8de38cb97f
Naksuuuu <ariel.233040088@mail.unpas.ac.id>
2024-02-19 20:26:58 +0700

    mengerjakan tugas 1

--- tugas/tugas1/1a.php
+++ tugas/tugas1/1a.php
@@ -1,6 +1,22 @@
 <?php
-// cek push github
+// inisialisasi variable
 
-echo "Ariel wijaya";
 
+$topi = "Topi";
+$bundar = "Bundar";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2><em> 
+    <?php echo '"' . $topi . " saya " . $bundar . ", " . $bundar . " " . $topi . " saya" . '"';  ?>
+    </em></h2>
+    
+</body>
+</html>
