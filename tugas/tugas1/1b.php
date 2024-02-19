0ff908eba7cfc43b123d63a16d7ce2d193ae80de
Naksuuuu <ariel.233040088@mail.unpas.ac.id>
2024-02-19 21:00:32 +0700

    mengerjakan tugas 1 b

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,17 @@
+<?php 
+
+$nrp = 88;
+
+$nrp *= 5;
+echo "Jika aku di kali 5, maka aku sekarang menjadi <b>$nrp</b><br> ";
+
+$nrp /= 2;
+echo "Jika aku di bagi 2, maka aku sekarang menjadi <b>$nrp</b><br> ";
+
+$nrp += 75;
+echo "Jika aku di tambah 75, maka aku sekarang menjadi <b>$nrp</b><br> ";
+
+$nrp -= 20;
+echo "Jika aku di kurang 20, maka aku sekarang menjadi <b>$nrp</b><br> ";
+
+?>
